d31abdd72855efec8562736dc766fbe3ba27fe13
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-19 12:18:15 +0100

    add method ClassMethod

--- Program.cs
+++ Program.cs
@@ -17,7 +17,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyClass a = new MyClass { myString = "Mario", myInteger = 20 };
+            Console.WriteLine(a);
+            MyStruct b = new MyStruct { myString = "Luigi", myInteger = 30 };
+            Console.WriteLine(b);
+            ClassMethod(a, b, 10, "Bello");
+
+        }
+        static void ClassMethod(MyClass a, MyStruct b, int x, string y)
+        {
+
+            a.myInteger = x;
+            Console.WriteLine(a);
+            b.myInteger = x;
+            Console.WriteLine(b);
+            a.myString = y;
+            Console.WriteLine(a);
+            b.myString = y;
+            Console.WriteLine(b);
+
+
         }
     }
 }

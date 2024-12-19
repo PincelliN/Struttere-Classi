fb002c7da5dbba4a1e3d0c9bd01359eab4c4dafc
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-19 12:01:51 +0100

    Add struct MyStruct

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 ﻿namespace Strutture
 {
-    class MyClass
+    public class MyClass
     {
         public string? myString;
         public int myInteger;
 
     }
+    public struct MyStruct
+    {
+        public string myString;
+        public int myInteger;
+    }
+
+
     class Program
     {
         static void Main(string[] args)
